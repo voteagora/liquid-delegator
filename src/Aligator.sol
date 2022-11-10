e238401783c4fcb6b1f53c3072e1238bc6edecce
w1nt3r-eth <w1nt3r.eth@gmail.com>
2022-11-10 15:53:03 -0800

    Settle on the rules design

--- src/Aligator.sol
+++ src/Aligator.sol
@@ -54,27 +54,28 @@ enum Clearance {
     None,
     Propose,
     Vote,
-    Execute
+    Sign,
+    Subdelegate,
+    Refund
 }
 
 struct Rules {
-    bool active; // 8
-    uint8 maxRedelegations; // 16
-    uint32 notValidBefore; // 48
-    uint32 notValidAfter; // 80
-    uint16 blocksBeforeVoteCloses; // 96
-    bool canSignProposals; // 104
-    bool canReceiveRefunds; // 112
-    uint32 maxEthValue; // 144
-    bool canCallContracts; // 152
-    //
+    uint8 permissions;
+    uint8 maxRedelegations;
+    uint32 notValidBefore;
+    uint32 notValidAfter;
+    uint16 blocksBeforeVoteCloses;
     address customRule;
 }
 
 contract AligatorWithRules {
     address public immutable owner;
-    IGovernorBravo public immutable governor;
+    INounsDAOV2 public immutable governor;
     mapping(address => mapping(address => Rules)) public subDelegations;
+
+    uint8 internal constant PERMISSION_PROPOSE = 0x01;
+    uint8 internal constant PERMISSION_VOTE = 0x02;
+    uint8 internal constant PERMISSION_SIGN = 0x04;
 
     bytes32 internal constant DOMAIN_TYPEHASH =
         keccak256("EIP712Domain(string name,uint256 chainId,address verifyingContract)");
@@ -84,24 +85,43 @@ contract AligatorWithRules {
     event SubDelegation(address indexed from, address indexed to, Rules rules);
     // Emit event when casting vote
 
-    error NotDelegated(address from, address to);
-    error ChainTooLong();
+    error BadSignature();
 
-    constructor(address _owner, IGovernorBravo _governor) {
+    error NotDelegated(address from, address to, uint8 permissions);
+    error NotValidYet(address from, address to, uint32 notValidBefore);
+    error NotValidAnymore(address from, address to, uint32 notValidAfter);
+    error TooEarly(address from, address to, uint32 blocksBeforeVoteCloses);
+
+    error ChainTooLong();
+    error InvalidCustomRule(address customRule);
+
+    constructor(address _owner, INounsDAOV2 _governor) {
         owner = _owner;
         governor = _governor;
     }
 
+    function propose(
+        address[] calldata authority,
+        address[] calldata targets,
+        uint256[] calldata values,
+        string[] calldata signatures,
+        bytes[] calldata calldatas,
+        string calldata description
+    ) external returns (uint256 proposalId) {
+        proposalId = governor.propose(targets, values, signatures, calldatas, description);
+        validate(msg.sender, authority, PERMISSION_PROPOSE, proposalId, 0xFF);
+    }
+
     function castVote(address[] calldata authority, uint256 proposalId, uint8 support) external {
-        validate(msg.sender, authority, proposalId, support);
+        validate(msg.sender, authority, PERMISSION_VOTE, proposalId, support);
         governor.castVote(proposalId, support);
     }
 
     function castVoteWithReason(address[] calldata authority, uint256 proposalId, uint8 support, string calldata reason)
         external
     {
-        validate(msg.sender, authority, proposalId, support);
-        INounsDAOV2(address(governor)).castVoteWithReason(proposalId, support, reason);
+        validate(msg.sender, authority, PERMISSION_VOTE, proposalId, support);
+        governor.castVoteWithReason(proposalId, support, reason);
     }
 
     function castVoteBySig(
@@ -118,8 +138,11 @@ contract AligatorWithRules {
         bytes32 digest = keccak256(abi.encodePacked("\x19\x01", domainSeparator, structHash));
         address signatory = ecrecover(digest, v, r, s);
 
-        require(signatory != address(0), "NounsDAO::castVoteBySig: invalid signature");
-        validate(signatory, authority, proposalId, support);
+        if (signatory != address(0)) {
+            revert BadSignature();
+        }
+
+        validate(signatory, authority, PERMISSION_VOTE, proposalId, support);
         governor.castVote(proposalId, support);
     }
 
@@ -128,36 +151,45 @@ contract AligatorWithRules {
         emit SubDelegation(msg.sender, to, rules);
     }
 
-    function validate(address sender, address[] calldata authority, uint256 proposalId, uint8 support) internal view {
+    function validate(
+        address sender,
+        address[] calldata authority,
+        uint8 permissions,
+        uint256 proposalId,
+        uint8 support
+    ) internal view {
         address account = owner;
 
         if (account == sender) {
             return;
         }
 
-        INounsDAOV2.ProposalCondensed memory proposal = INounsDAOV2(address(governor)).proposals(proposalId);
-        (address[] memory targets, uint256[] memory values, string[] memory signatures, bytes[] memory calldatas) =
-            INounsDAOV2(address(governor)).getActions(proposalId);
+        INounsDAOV2.ProposalCondensed memory proposal = governor.proposals(proposalId);
 
         for (uint256 i = 0; i < authority.length; i++) {
             address to = authority[i];
             Rules memory rules = subDelegations[account][to];
-            if (!rules.active) {
-                revert NotDelegated(account, to);
+
+            if (rules.permissions & permissions != permissions) {
+                revert NotDelegated(account, to, permissions);
             }
+            // TODO: check redelegations limit
             if (block.timestamp < rules.notValidBefore) {
-                revert NotDelegated(account, to);
+                revert NotValidYet(account, to, rules.notValidBefore);
             }
-            if (block.timestamp > rules.notValidAfter) {
-                revert NotDelegated(account, to);
+            if (rules.notValidAfter != 0 && block.timestamp > rules.notValidAfter) {
+                revert NotValidAnymore(account, to, rules.notValidAfter);
             }
-            if (proposal.endBlock - block.number > rules.blocksBeforeVoteCloses) {
-                revert NotDelegated(account, to);
+            if (rules.blocksBeforeVoteCloses != 0 && proposal.endBlock - block.number > rules.blocksBeforeVoteCloses) {
+                revert TooEarly(account, to, rules.blocksBeforeVoteCloses);
             }
             if (rules.customRule != address(0)) {
                 bytes4 selector = IRule(rules.customRule).validate(sender, proposalId, support);
-                require(selector == IRule.validate.selector, "Invalid custom rule");
+                if (selector != IRule.validate.selector) {
+                    revert InvalidCustomRule(rules.customRule);
+                }
             }
+
             account = to;
         }
 
@@ -165,16 +197,16 @@ contract AligatorWithRules {
             return;
         }
 
-        revert NotDelegated(account, sender);
+        revert NotDelegated(account, sender, permissions);
     }
 }
 
 contract AligatorFactory {
-    IGovernorBravo public immutable governor;
+    INounsDAOV2 public immutable governor;
 
     event AligatorDeployed(address indexed owner, address aligator);
 
-    constructor(IGovernorBravo _governor) {
+    constructor(INounsDAOV2 _governor) {
         governor = _governor;
     }
 
@@ -184,177 +216,3 @@ contract AligatorFactory {
         emit AligatorDeployed(owner, address(aligator));
     }
 }
-
-contract AligatorOffchainAuthority {
-    address public immutable owner;
-    IGovernorBravo public immutable governor;
-    mapping(address => mapping(address => bytes)) public subDelegations;
-
-    event SubDelegation(address indexed from, address indexed to, bytes rules);
-    // Emit event when casting vote
-
-    error NotDelegated(address from, address to);
-
-    constructor(IGovernorBravo _governor) {
-        owner = msg.sender;
-        governor = _governor;
-    }
-
-    function castVote(address[] calldata authority, uint256 proposalId, uint8 support) external {
-        require(canVote(authority));
-        governor.castVote(proposalId, support);
-    }
-
-    function subDelegate(address account, bytes calldata rules) external {
-        subDelegations[msg.sender][account] = rules;
-        emit SubDelegation(msg.sender, account, rules);
-    }
-
-    function canVote(address[] calldata authority) internal view returns (bool) {
-        address account = owner;
-        for (uint256 i = 0; i < authority.length; i++) {
-            address to = authority[i];
-            if (subDelegations[account][to].length == 0) {
-                revert NotDelegated(account, to);
-            }
-            account = to;
-        }
-
-        return account == msg.sender;
-    }
-}
-
-contract CanVoteOnlyBeforeTimestamp is IRule {
-    uint256 public immutable timestamp;
-
-    constructor(uint256 _timestamp) {
-        timestamp = _timestamp;
-    }
-
-    function check(address voter, uint256 proposalId, uint8 support, bytes calldata rule)
-        external
-        view
-        override
-        returns (bool)
-    {
-        return block.timestamp <= timestamp;
-    }
-}
-
-contract CanVoteOnlyWithinBlocks is IRule {
-    INounsDAOV2 private immutable dao;
-    uint256 private immutable blocks;
-
-    constructor(INounsDAOV2 _dao, uint256 _blocks) {
-        dao = _dao;
-        blocks = _blocks;
-    }
-
-    function check(address voter, uint256 proposalId, uint8 support, bytes calldata rule)
-        external
-        view
-        override
-        returns (bool)
-    {
-        INounsDAOV2.ProposalCondensed memory proposal = dao.proposals(proposalId);
-        return proposal.endBlock + blocks >= block.number;
-    }
-}
-
-contract ValueLimit is IRule {
-    INounsDAOV2 private immutable dao;
-    uint256 public immutable limit;
-
-    constructor(INounsDAOV2 _dao, uint256 _limit) {
-        dao = _dao;
-        limit = _limit;
-    }
-
-    function check(address voter, uint256 proposalId, uint8 support, bytes calldata rule)
-        external
-        view
-        override
-        returns (bool)
-    {
-        (, uint256[] memory values,,) = dao.getActions(proposalId);
-        uint256 total = 0;
-        for (uint256 i = 0; i < values.length; i++) {
-            total += values[i];
-        }
-        return total <= limit;
-    }
-}
-
-contract NoUpgrades is IRule {
-    INounsDAOV2 private immutable dao;
-
-    constructor(INounsDAOV2 _dao) {
-        dao = _dao;
-    }
-
-    function check(address voter, uint256 proposalId, uint8 support, bytes calldata rule)
-        external
-        view
-        override
-        returns (bool)
-    {
-        (address[] memory targets,,,) = dao.getActions(proposalId);
-        for (uint256 i = 0; i < targets.length; i++) {
-            if (targets[i] == address(dao)) {
-                return false;
-            }
-        }
-        return true;
-    }
-}
-
-contract AligatorOnchainAuthority {
-    address public immutable owner;
-    IGovernorBravo public immutable governor;
-
-    mapping(address => address[]) public subDelegations; // from => to[]
-    mapping(address => uint256) public canVote;
-
-    event SubDelegation(address indexed from, address indexed to, bool indexed status);
-
-    constructor(IGovernorBravo _governor) {
-        owner = msg.sender;
-        governor = _governor;
-        canVote[msg.sender] = 1;
-    }
-
-    function castVote(uint256 proposalId, uint8 support) external {
-        require(canVote[msg.sender] > 0);
-        governor.castVote(proposalId, support);
-    }
-
-    function subDelegate(address account, bool status) external {
-        require(canVote[msg.sender] > 0);
-
-        if (status) {
-            canVote[account] += 1;
-            subDelegations[msg.sender].push(account);
-        } else {
-            undelegate(account);
-        }
-    }
-
-    function undelegate(address account) internal {
-        for (uint256 i = 0; i < subDelegations[account].length; i++) {
-            address to = subDelegations[account][i];
-            undelegate(to);
-            canVote[to]--;
-        }
-        delete subDelegations[account];
-    }
-}
-
-// Simple delegation
-// Q1: A -> B -> C, can A and B vote? Yes
-// Q2: A -> B, A -> C, can both B and C vote? Yes
-
-// Constraints
-// Q3: A -> B (max 100e) -> C (7 days). C can only vote for 7 days and less than 100e
-// Q4: Can it be multiple constraints? Yes. AND, OR, etc.? No
-// Q5: Diamond: A -> B (max 100e), A -> C (7 days), B -> D, C -> D. D can only vote for 7 days OR less than 100e
-// Q6: On-chain vs off-chain. Diamon scenario + constraints
--- test/Aligator.t.sol
+++ test/Aligator.t.sol
@@ -3,16 +3,19 @@ pragma solidity ^0.8.13;
 
 import "forge-std/Test.sol";
 import {IGovernorBravo} from "../src/interfaces/IGovernorBravo.sol";
+import {INounsDAOV2} from "../src/interfaces/INounsDAOV2.sol";
 import "../src/Aligator.sol";
 import "./Utils.sol";
 
 contract AligatorTest is Test {
-    AligatorOffchainAuthority public aligator;
+    AligatorFactory public factory;
+    AligatorWithRules public aligator;
     NounsDAO public nounsDAO;
 
     function setUp() public {
         nounsDAO = new NounsDAO();
-        aligator = new AligatorOffchainAuthority(IGovernorBravo(address(nounsDAO)));
+        factory = new AligatorFactory(nounsDAO);
+        aligator = factory.create(address(this));
     }
 
     function testVote() public {
@@ -24,7 +27,16 @@ contract AligatorTest is Test {
         address[] memory authority = new address[](1);
         authority[0] = address(Utils.alice);
 
-        aligator.subDelegate(Utils.alice, "1");
+        Rules memory rules = Rules({
+            permissions: 0x02,
+            maxRedelegations: 1,
+            notValidBefore: 0,
+            notValidAfter: 0,
+            blocksBeforeVoteCloses: 0,
+            customRule: address(0)
+        });
+
+        aligator.subDelegate(Utils.alice, rules);
         vm.prank(Utils.alice);
         aligator.castVote(authority, 1, 1);
     }
@@ -35,11 +47,20 @@ contract AligatorTest is Test {
         authority[1] = address(Utils.bob);
         authority[2] = address(Utils.carol);
 
-        aligator.subDelegate(Utils.alice, "1");
+        Rules memory rules = Rules({
+            permissions: 0x02,
+            maxRedelegations: 1,
+            notValidBefore: 0,
+            notValidAfter: 0,
+            blocksBeforeVoteCloses: 0,
+            customRule: address(0)
+        });
+
+        aligator.subDelegate(Utils.alice, rules);
         vm.prank(Utils.alice);
-        aligator.subDelegate(Utils.bob, "1");
+        aligator.subDelegate(Utils.bob, rules);
         vm.prank(Utils.bob);
-        aligator.subDelegate(Utils.carol, "1");
+        aligator.subDelegate(Utils.carol, rules);
 
         vm.prank(Utils.carol);
         aligator.castVote(authority, 1, 1);
@@ -51,14 +72,33 @@ contract AligatorTest is Test {
         authority[1] = address(Utils.bob);
         authority[2] = address(Utils.carol);
 
-        aligator.subDelegate(Utils.alice, "1");
+        Rules memory rules = Rules({
+            permissions: 0x02,
+            maxRedelegations: 1,
+            notValidBefore: 0,
+            notValidAfter: 0,
+            blocksBeforeVoteCloses: 0,
+            customRule: address(0)
+        });
+
+        aligator.subDelegate(Utils.alice, rules);
         vm.prank(Utils.alice);
-        aligator.subDelegate(Utils.bob, "1");
+        aligator.subDelegate(Utils.bob, rules);
         vm.prank(Utils.bob);
-        aligator.subDelegate(Utils.carol, "1");
+        aligator.subDelegate(Utils.carol, rules);
 
         vm.prank(Utils.alice);
-        aligator.subDelegate(Utils.bob, "");
+        aligator.subDelegate(
+            Utils.bob,
+            Rules({
+                permissions: 0,
+                maxRedelegations: 0,
+                notValidBefore: 0,
+                notValidAfter: 0,
+                blocksBeforeVoteCloses: 0,
+                customRule: address(0)
+            })
+        );
 
         vm.prank(Utils.carol);
         vm.expectRevert();
@@ -66,6 +106,45 @@ contract AligatorTest is Test {
     }
 }
 
-contract NounsDAO {
+contract NounsDAO is INounsDAOV2 {
+    function quorumVotes() external view returns (uint256) {}
+
+    function votingDelay() external view returns (uint256) {}
+
+    function votingPeriod() external view returns (uint256) {}
+
+    function propose(
+        address[] memory targets,
+        uint256[] memory values,
+        string[] memory signatures,
+        bytes[] memory calldatas,
+        string memory description
+    ) external returns (uint256) {}
+
     function castVote(uint256 proposalId, uint8 support) external {}
+
+    function queue(uint256 proposalId) external {}
+
+    function execute(uint256 proposalId) external {}
+
+    function castRefundableVote(uint256 proposalId, uint8 support) external {}
+
+    function castRefundableVoteWithReason(uint256 proposalId, uint8 support, string calldata reason) external {}
+
+    function castVoteWithReason(uint256 proposalId, uint8 support, string calldata reason) external {}
+
+    function castVoteBySig(uint256 proposalId, uint8 support, uint8 v, bytes32 r, bytes32 s) external {}
+
+    function proposals(uint256 proposalId) external view returns (ProposalCondensed memory) {}
+
+    function getActions(uint256 proposalId)
+        external
+        view
+        returns (
+            address[] memory targets,
+            uint256[] memory values,
+            string[] memory signatures,
+            bytes[] memory calldatas
+        )
+    {}
 }
