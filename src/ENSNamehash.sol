e5d11a1a9fd49e689684d3034cb5a2714d38d917
w1nt3r-eth <w1nt3r.eth@gmail.com>
2023-02-17 13:22:51 -0800

    Add ENS forward lookup tests

---
+++ src/ENSNamehash.sol
@@ -0,0 +1,41 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+// Imported from https://github.com/JonahGroendal/ens-namehash
+
+/*
+ * @dev Solidity implementation of the ENS namehash algorithm.
+ *
+ * Warning! Does not normalize or validate names before hashing.
+ */
+
+library ENSNamehash {
+    function namehash(bytes memory domain) internal pure returns (bytes32) {
+        return namehash(domain, 0);
+    }
+
+    function namehash(bytes memory domain, uint256 i) internal pure returns (bytes32) {
+        if (domain.length <= i) {
+            return 0x0000000000000000000000000000000000000000000000000000000000000000;
+        }
+
+        uint256 len = LabelLength(domain, i);
+
+        return keccak256(abi.encodePacked(namehash(domain, i + len + 1), keccak(domain, i, len)));
+    }
+
+    function LabelLength(bytes memory domain, uint256 i) private pure returns (uint256) {
+        uint256 len;
+        while (i + len != domain.length && domain[i + len] != 0x2e) {
+            len++;
+        }
+        return len;
+    }
+
+    function keccak(bytes memory data, uint256 offset, uint256 len) private pure returns (bytes32 ret) {
+        require(offset + len <= data.length);
+        assembly {
+            ret := keccak256(add(add(data, 32), offset), len)
+        }
+    }
+}
--- test/AlligatorENS.t.sol
+++ test/AlligatorENS.t.sol
@@ -2,35 +2,40 @@
 pragma solidity ^0.8.13;
 
 import "forge-std/Test.sol";
-import {IERC1271} from "@openzeppelin/contracts/interfaces/IERC1271.sol";
-import {IGovernorBravo} from "../src/interfaces/IGovernorBravo.sol";
-import {INounsDAOV2} from "../src/interfaces/INounsDAOV2.sol";
-import "../src/Alligator.sol";
-import "./Utils.sol";
 import {ENS} from "ens-contracts/registry/ENS.sol";
+import {ENSNamehash} from "../src/ENSNamehash.sol";
+import {INounsDAOV2} from "../src/interfaces/INounsDAOV2.sol";
+import {Resolver} from "ens-contracts/resolvers/Resolver.sol";
+import "../src/Alligator.sol";
 
 contract AlligatorENSTest is Test {
     address immutable w1nt3r = 0x1E79b045Dc29eAe9fdc69673c9DCd7C53E5E159D;
+    ENS immutable ens = ENS(0x00000000000C2E074eC69A0dFb2997BA6C7d2e1e);
 
     function testENSName() public {
         string memory MAINNET_RPC_URL = vm.envString("MAINNET_RPC_URL");
         vm.createSelectFork(MAINNET_RPC_URL, 16651000);
 
-        bytes32 ensNameHash = 0x4898f9bb4f8f73eed4f7f71ecd16fbe6a0fd0b4ee5fd732b8b225c5f0ea16524;
+        bytes32 ensNameHash = ENSNamehash.namehash("al.w1nt3r.eth");
         Alligator alligator = new Alligator(INounsDAOV2(address(0)), "al.w1nt3r.eth", ensNameHash);
 
         vm.prank(w1nt3r);
-        ENS(0x00000000000C2E074eC69A0dFb2997BA6C7d2e1e).setSubnodeOwner(
-            0x7c4c4f589b652b90fcb7a27147ab6b3b0674c65e279c60aa9297e8041da23589,
-            0x4677be8c780480a69d828bc59996257213298f29b0b05a92f2225875b06d85be,
-            address(alligator)
-        );
+        ens.setSubnodeOwner(ENSNamehash.namehash("w1nt3r.eth"), keccak256(abi.encodePacked("al")), address(alligator));
 
         address proxy1 = alligator.create(address(this));
         assertEq(lookupReverseName(proxy1), "1.al.w1nt3r.eth");
+        assertEq(lookupAddress("1.al.w1nt3r.eth"), proxy1);
 
         address proxy2 = alligator.create(w1nt3r);
         assertEq(lookupReverseName(proxy2), "2.al.w1nt3r.eth");
+        assertEq(lookupAddress("2.al.w1nt3r.eth"), proxy2);
+    }
+
+    function lookupAddress(string memory name) internal view returns (address) {
+        bytes32 node = ENSNamehash.namehash(bytes(name));
+        address resolver = ens.resolver(node);
+        Resolver ensResolver = Resolver(resolver);
+        return ensResolver.addr(node);
     }
 
     function lookupReverseName(address addr) internal view returns (string memory) {
