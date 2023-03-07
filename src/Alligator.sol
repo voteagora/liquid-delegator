049e119fdc492316e6aa1ca45f5e8aaac31ae3cc
jj_ranalli <jacopo.ranalli@mail.polimi.it>
2023-03-07 02:52:15 +0100

    refactor

--- src/Alligator.sol
+++ src/Alligator.sol
@@ -1,51 +1,13 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import "./interfaces/IAlligator.sol";
+import {Proxy} from "./Proxy.sol";
+import {ENSHelper} from "./ENSHelper.sol";
+import {ECDSA} from "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
 import {INounsDAOV2} from "./interfaces/INounsDAOV2.sol";
 import {IRule} from "./interfaces/IRule.sol";
-import {ENSHelper} from "./ENSHelper.sol";
 import {IERC1271} from "@openzeppelin/contracts/interfaces/IERC1271.sol";
-import {ECDSA} from "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
-
-contract Proxy is IERC1271 {
-    address internal immutable alligator;
-    address internal immutable governor;
-
-    constructor(address _governor) {
-        alligator = msg.sender;
-        governor = _governor;
-    }
-
-    function isValidSignature(bytes32 hash, bytes calldata signature) external view override returns (bytes4) {
-        return IAlligator(alligator).isValidProxySignature(address(this), hash, signature);
-    }
-
-    function setENSReverseRecord(string calldata name) external {
-        require(msg.sender == alligator);
-        IENSReverseRegistrar(0x084b1c3C81545d370f3634392De611CaaBFf8148).setName(name);
-    }
-
-    fallback() external payable {
-        require(msg.sender == alligator);
-        address addr = governor;
-
-        assembly {
-            calldatacopy(0, 0, calldatasize())
-            let result := call(gas(), addr, callvalue(), 0, calldatasize(), 0, 0)
-            returndatacopy(0, 0, returndatasize())
-            switch result
-            case 0 {
-                revert(0, returndatasize())
-            }
-            default {
-                return(0, returndatasize())
-            }
-        }
-    }
-
-    // `receive` is omitted to minimize contract size
-}
+import "./interfaces/IAlligator.sol";
 
 contract Alligator is IAlligator, ENSHelper {
     // =============================================================
@@ -501,8 +463,4 @@ contract Alligator is IAlligator, ENSHelper {
     function min(uint256 a, uint256 b) internal pure returns (uint256) {
         return a < b ? a : b;
     }
-}
-
-interface IENSReverseRegistrar {
-    function setName(string memory name) external returns (bytes32 node);
 }
