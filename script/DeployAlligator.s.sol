097eb847b5798047f3d41f3de8b2b6d0a81a30bb
jj_ranalli <jacopo.ranalli@mail.polimi.it>
2023-03-13 12:07:45 +0100

    cleanup deploy script

--- script/DeployAlligator.s.sol
+++ script/DeployAlligator.s.sol
@@ -2,22 +2,12 @@
 pragma solidity ^0.8.13;
 
 import "forge-std/Script.sol";
-import {IProxyRegistry} from "noun-contracts/external/opensea/IProxyRegistry.sol";
-import {NounsDAOExecutor} from "noun-contracts/governance/NounsDAOExecutor.sol";
-import {NounsDAOLogicV2} from "noun-contracts/governance/NounsDAOLogicV2.sol";
-import {NounsDAOProxyV2} from "noun-contracts/governance/NounsDAOProxyV2.sol";
-import {NounsDAOStorageV2} from "noun-contracts/governance/NounsDAOInterfaces.sol";
-import {NounsDescriptor} from "noun-contracts/NounsDescriptor.sol";
-import {FreeNounsTonken} from "./FreeNounsToken.sol";
-import {NounsSeeder} from "noun-contracts/NounsSeeder.sol";
-import {Alligator, Rules} from "../src/Alligator.sol";
+import {Alligator} from "../src/Alligator.sol";
 import {INounsDAOV2} from "../src/interfaces/INounsDAOV2.sol";
-import {DescriptorImageData} from "./DescriptorImageData.sol";
 import {ENSNamehash} from "../src/utils/ENSNamehash.sol";
 
 contract DeployAlligatorScript is Script {
     function run() public returns (Alligator alligator) {
-        // bytes32 salt = keccak256(bytes(vm.envString("SALT")));
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY"); // 0x0699919b235555Be219552912D4a992774e7FB2b
         address owner = 0x75a3A0d9e5aa246976e8B5775b224Efb3f9b2f9e;
 
@@ -25,7 +15,7 @@ contract DeployAlligatorScript is Script {
 
         // INounsDAOV2 nounsDAO = INounsDAOV2(0xD08faCeb444dbb6b063a51C2ddFb564Fa0f8Dce0); // GOERLI
         INounsDAOV2 nounsDAO = INounsDAOV2(0x6f3E6272A167e8AcCb32072d08E0957F9c79223d); // MAINNET
-        string memory ensName = "nouns.voteagora.eth"; // or nouns.voteagora.eth
+        string memory ensName = "nouns.voteagora.eth";
         bytes32 ensNameHash = ENSNamehash.namehash(bytes(ensName));
 
         alligator = new Alligator(nounsDAO, ensName, ensNameHash);
