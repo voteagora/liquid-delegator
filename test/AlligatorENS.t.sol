608b564d345927cef5e321be7947229c6eeb6039
jj_ranalli <jacopo.ranalli@mail.polimi.it>
2023-03-15 19:26:07 +0100

    added ENS test for V2

--- test/AlligatorENS.t.sol
+++ test/AlligatorENS.t.sol
@@ -7,41 +7,93 @@ import {ENSNamehash} from "../src/utils/ENSNamehash.sol";
 import {INounsDAOV2} from "../src/interfaces/INounsDAOV2.sol";
 import {Resolver} from "ens-contracts/resolvers/Resolver.sol";
 import "../src/Alligator.sol";
+import "../src/v2/AlligatorV2.sol";
 import "./Utils.sol";
 
 contract AlligatorENSTest is Test {
     error AlreadyRegistered();
 
-    address immutable w1nt3r = 0x1E79b045Dc29eAe9fdc69673c9DCd7C53E5E159D;
+    address immutable ensOwner = 0x63FD9D5c51adB4b41629608385Bb2AD05FC63A20;
     ENS immutable ens = ENS(0x00000000000C2E074eC69A0dFb2997BA6C7d2e1e);
+    Rules public baseRules =
+        Rules(
+            7, // All permissions
+            255, // Max redelegations
+            0,
+            0,
+            0,
+            address(0)
+        );
 
-    function testENSName() public {
+    function testENSNameV1() public {
         string memory MAINNET_RPC_URL = vm.envString("MAINNET_RPC_URL");
         vm.createSelectFork(MAINNET_RPC_URL, 16651000);
 
-        bytes32 ensNameHash = ENSNamehash.namehash("al.w1nt3r.eth");
-        Alligator alligator = new Alligator(INounsDAOV2(address(0)), "al.w1nt3r.eth", ensNameHash);
+        bytes32 ensNameHash = ENSNamehash.namehash("v1.voteagora.eth");
+        Alligator alligator = new Alligator(INounsDAOV2(address(0)), "v1.voteagora.eth", ensNameHash);
 
-        vm.prank(w1nt3r);
-        ens.setSubnodeOwner(ENSNamehash.namehash("w1nt3r.eth"), keccak256(abi.encodePacked("al")), address(alligator));
+        vm.prank(ensOwner);
+        ens.setSubnodeOwner(
+            ENSNamehash.namehash("voteagora.eth"),
+            keccak256(abi.encodePacked("v1")),
+            address(alligator)
+        );
 
         address proxy1 = alligator.create(address(this), true);
-        assertEq(lookupReverseName(proxy1), "1.al.w1nt3r.eth");
-        assertEq(lookupAddress("1.al.w1nt3r.eth"), proxy1);
+        assertEq(lookupReverseName(proxy1), "1.v1.voteagora.eth");
+        assertEq(lookupAddress("1.v1.voteagora.eth"), proxy1);
 
         address proxy2 = alligator.create(Utils.alice, false);
         assertEq(lookupReverseName(proxy2), "");
 
-        address proxy3 = alligator.create(w1nt3r, true);
-        assertEq(lookupReverseName(proxy3), "2.al.w1nt3r.eth");
-        assertEq(lookupAddress("2.al.w1nt3r.eth"), proxy3);
+        address proxy3 = alligator.create(ensOwner, true);
+        assertEq(lookupReverseName(proxy3), "2.v1.voteagora.eth");
+        assertEq(lookupAddress("2.v1.voteagora.eth"), proxy3);
 
         alligator.registerProxyDeployment(Utils.alice);
-        assertEq(lookupReverseName(proxy2), "3.al.w1nt3r.eth");
-        assertEq(lookupAddress("3.al.w1nt3r.eth"), proxy2);
+        assertEq(lookupReverseName(proxy2), "3.v1.voteagora.eth");
+        assertEq(lookupAddress("3.v1.voteagora.eth"), proxy2);
 
         vm.expectRevert(AlreadyRegistered.selector);
         alligator.registerProxyDeployment(address(this));
+    }
+
+    function testENSNameV2() public {
+        string memory MAINNET_RPC_URL = vm.envString("MAINNET_RPC_URL");
+        vm.createSelectFork(MAINNET_RPC_URL, 16651000);
+
+        bytes32 ensNameHash = ENSNamehash.namehash("v2.voteagora.eth");
+        AlligatorV2 alligator = new AlligatorV2(
+            INounsDAOV2(address(0)),
+            "v2.voteagora.eth",
+            ensNameHash,
+            address(this)
+        );
+
+        vm.prank(ensOwner);
+        ens.setSubnodeOwner(
+            ENSNamehash.namehash("voteagora.eth"),
+            keccak256(abi.encodePacked("v2")),
+            address(alligator)
+        );
+
+        address proxy1 = alligator.create(address(this), baseRules, true);
+        assertEq(lookupReverseName(proxy1), "1.v2.voteagora.eth");
+        assertEq(lookupAddress("1.v2.voteagora.eth"), proxy1);
+
+        address proxy2 = alligator.create(Utils.alice, baseRules, false);
+        assertEq(lookupReverseName(proxy2), "");
+
+        address proxy3 = alligator.create(ensOwner, baseRules, true);
+        assertEq(lookupReverseName(proxy3), "2.v2.voteagora.eth");
+        assertEq(lookupAddress("2.v2.voteagora.eth"), proxy3);
+
+        alligator.registerProxyDeployment(Utils.alice, baseRules);
+        assertEq(lookupReverseName(proxy2), "3.v2.voteagora.eth");
+        assertEq(lookupAddress("3.v2.voteagora.eth"), proxy2);
+
+        vm.expectRevert(AlreadyRegistered.selector);
+        alligator.registerProxyDeployment(address(this), baseRules);
     }
 
     function lookupAddress(string memory name) internal view returns (address) {
