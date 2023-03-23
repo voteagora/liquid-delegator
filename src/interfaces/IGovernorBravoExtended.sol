066793dd8e062ce1508e88f76ae131a8ec5bd586
jj_ranalli <jacopo.ranalli@mail.polimi.it>
2023-03-23 14:08:16 +0100

    fixed GovernorBravoMock mock and IGovernorBravoExtended interface

--- src/interfaces/IGovernorBravoExtended.sol
+++ src/interfaces/IGovernorBravoExtended.sol
@@ -5,6 +5,7 @@ import {IGovernorBravo} from "./IGovernorBravo.sol";
 
 // IGovernorBravo extended with castRefundableVote logic
 interface IGovernorBravoExtended is IGovernorBravo {
+    // Modified GovernorBravo Proposal struct to account for mapping and arrays being omitted in default mapping getters.
     struct Proposal {
         /// @notice Unique id for looking up a proposal
         uint id;
@@ -12,14 +13,6 @@ interface IGovernorBravoExtended is IGovernorBravo {
         address proposer;
         /// @notice The timestamp that the proposal will be available for execution, set once the vote succeeds
         uint eta;
-        /// @notice the ordered list of target addresses for calls to be made
-        address[] targets;
-        /// @notice The ordered list of values (i.e. msg.value) to be passed to the calls to be made
-        uint[] values;
-        /// @notice The ordered list of function signatures to be called
-        string[] signatures;
-        /// @notice The ordered list of calldata to be passed to each call
-        bytes[] calldatas;
         /// @notice The block at which voting begins: holders must delegate their votes prior to this block
         uint startBlock;
         /// @notice The block at which voting ends: votes must be cast prior to this block
@@ -34,19 +27,6 @@ interface IGovernorBravoExtended is IGovernorBravo {
         bool canceled;
         /// @notice Flag marking whether the proposal has been executed
         bool executed;
-        /// @notice Receipts of ballots for the entire set of voters
-        /// @dev omitted to allow compile
-        // mapping(address => Receipt) receipts;
-    }
-
-    /// @notice Ballot receipt record for a voter
-    struct Receipt {
-        /// @notice Whether or not a vote has been cast
-        bool hasVoted;
-        /// @notice Whether or not the voter supports the proposal or abstains
-        uint8 support;
-        /// @notice The number of votes the voter had, which were cast
-        uint96 votes;
     }
 
     function proposals(uint256 proposalId) external view returns (Proposal memory);
--- test/mock/GovernorBravoMock.sol
+++ test/mock/GovernorBravoMock.sol
@@ -1,13 +1,55 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import {IGovernorBravoExtended} from "src/interfaces/IGovernorBravoExtended.sol";
 import {IGovernorMock} from "./IGovernorMock.sol";
 
-contract GovernorBravoMock is IGovernorBravoExtended, IGovernorMock {
+contract GovernorBravoMock is IGovernorMock {
     /// @notice Emitted when a voter cast a vote requesting a gas refund.
     event RefundableVote(address indexed voter, uint256 refundAmount, bool refundSent);
     event VoteCast(address voter, uint256 proposalId, uint8 support, uint256 votes);
+
+    struct Proposal {
+        /// @notice Unique id for looking up a proposal
+        uint id;
+        /// @notice Creator of the proposal
+        address proposer;
+        /// @notice The timestamp that the proposal will be available for execution, set once the vote succeeds
+        uint eta;
+        /// @notice the ordered list of target addresses for calls to be made
+        address[] targets;
+        /// @notice The ordered list of values (i.e. msg.value) to be passed to the calls to be made
+        uint[] values;
+        /// @notice The ordered list of function signatures to be called
+        string[] signatures;
+        /// @notice The ordered list of calldata to be passed to each call
+        bytes[] calldatas;
+        /// @notice The block at which voting begins: holders must delegate their votes prior to this block
+        uint startBlock;
+        /// @notice The block at which voting ends: votes must be cast prior to this block
+        uint endBlock;
+        /// @notice Current number of votes in favor of this proposal
+        uint forVotes;
+        /// @notice Current number of votes in opposition to this proposal
+        uint againstVotes;
+        /// @notice Current number of votes for abstaining for this proposal
+        uint abstainVotes;
+        /// @notice Flag marking whether the proposal has been canceled
+        bool canceled;
+        /// @notice Flag marking whether the proposal has been executed
+        bool executed;
+        /// @notice Receipts of ballots for the entire set of voters
+        mapping(address => Receipt) receipts;
+    }
+
+    /// @notice Ballot receipt record for a voter
+    struct Receipt {
+        /// @notice Whether or not a vote has been cast
+        bool hasVoted;
+        /// @notice Whether or not the voter supports the proposal or abstains
+        uint8 support;
+        /// @notice The number of votes the voter had, which were cast
+        uint96 votes;
+    }
 
     /// @notice The maximum priority fee used to cap gas refunds in `castRefundableVote`
     uint256 public constant MAX_REFUND_PRIORITY_FEE = 2 gwei;
@@ -26,6 +68,14 @@ contract GovernorBravoMock is IGovernorBravoExtended, IGovernorMock {
     uint256 public lastSupport;
     uint256 public totalVotes;
     mapping(address => bool) public hasVoted;
+
+    /// @notice The official record of all proposals ever proposed
+    mapping(uint256 => Proposal) public proposals;
+
+    constructor() {
+        // Set end block for proposal[1] for `testRevert_validate_TooEarly` test
+        proposals[1].endBlock = 101;
+    }
 
     function quorumVotes() external view returns (uint256) {}
 
@@ -54,26 +104,9 @@ contract GovernorBravoMock is IGovernorBravoExtended, IGovernorMock {
 
     function execute(uint256 proposalId) external {}
 
-    function castRefundableVote(uint256 proposalId, uint8 support) external {}
-
-    function castRefundableVoteWithReason(uint256 proposalId, uint8 support, string calldata) external {
-        require(support <= 2, "NounsDAO::castVoteInternal: invalid vote type");
-        require(hasVoted[msg.sender] == false, "NounsDAO::castVoteInternal: voter already voted");
-        uint256 startGas = gasleft();
-
-        lastVoter = msg.sender;
-        lastProposalId = proposalId;
-        lastSupport = support;
-        totalVotes += 1;
-        hasVoted[msg.sender] = true;
-        emit VoteCast(msg.sender, proposalId, support, 0);
-
-        _refundGas(startGas);
-    }
-
     function castVoteWithReason(uint256 proposalId, uint8 support, string calldata) external {
-        require(support <= 2, "NounsDAO::castVoteInternal: invalid vote type");
-        require(hasVoted[msg.sender] == false, "NounsDAO::castVoteInternal: voter already voted");
+        require(support <= 2, "castVoteInternal: invalid vote type");
+        require(hasVoted[msg.sender] == false, "castVoteInternal: voter already voted");
 
         lastVoter = msg.sender;
         lastProposalId = proposalId;
@@ -84,10 +117,6 @@ contract GovernorBravoMock is IGovernorBravoExtended, IGovernorMock {
     }
 
     function castVoteBySig(uint256 proposalId, uint8 support, uint8 v, bytes32 r, bytes32 s) external {}
-
-    function proposals(uint256) external view returns (Proposal memory proposal) {
-        proposal.endBlock = block.number + 100;
-    }
 
     function getActions(
         uint256 proposalId
@@ -101,6 +130,27 @@ contract GovernorBravoMock is IGovernorBravoExtended, IGovernorMock {
             bytes[] memory calldatas
         )
     {}
+
+    // =============================================================
+    //                           REFUND LOGIC
+    // =============================================================
+
+    function castRefundableVote(uint256 proposalId, uint8 support) external {}
+
+    function castRefundableVoteWithReason(uint256 proposalId, uint8 support, string calldata) external {
+        require(support <= 2, "castVoteInternal: invalid vote type");
+        require(hasVoted[msg.sender] == false, "castVoteInternal: voter already voted");
+        uint256 startGas = gasleft();
+
+        lastVoter = msg.sender;
+        lastProposalId = proposalId;
+        lastSupport = support;
+        totalVotes += 1;
+        hasVoted[msg.sender] = true;
+        emit VoteCast(msg.sender, proposalId, support, 0);
+
+        _refundGas(startGas);
+    }
 
     function _refundGas(uint256 startGas) internal {
         unchecked {
