8f7498b2febc13baeb5b4f08a50ae14032eecc65
jj_ranalli <jacopo.ranalli@mail.polimi.it>
2023-03-23 11:47:45 +0100

    extension tests wip

---
+++ test/AlligatorV2Open.t.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {AlligatorV2Open} from "src/v2/extensions/AlligatorV2Open.sol";
+import "./utils/AlligatorV2Base.sol";
+import "./mock/OpenMock.sol";
+
+contract AlligatorV2OpenTest is AlligatorV2Base {
+    // =============================================================
+    //                             TESTS
+    // =============================================================
+
+    function setUp() public override {
+        SetupV2.setUp();
+
+        governor = new OpenMock();
+        alligator = AlligatorV2(
+            payable(
+                _create3Factory.deploy(
+                    keccak256(bytes("SALT")),
+                    bytes.concat(
+                        type(AlligatorV2Open).creationCode,
+                        abi.encode(address(governor), "", 0, address(this))
+                    )
+                )
+            )
+        );
+        root = alligator.create(address(this), baseRules, true); // selfProxy
+    }
+}
