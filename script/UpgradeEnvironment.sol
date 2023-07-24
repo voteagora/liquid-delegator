3b8ff55ceff5bd9d009a887702468466da6ee4ad
jj_ranalli <jacopo.ranalli@mail.polimi.it>
2023-07-24 21:02:15 +0200

    fix: UpgradeEnvironment

--- script/UpgradeEnvironment.sol
+++ script/UpgradeEnvironment.sol
@@ -7,56 +7,42 @@ import {IProxyRegistry} from "noun-contracts/external/opensea/IProxyRegistry.sol
 import {NounsDAOStorageV2} from "noun-contracts/governance/NounsDAOInterfaces.sol";
 import {NounsDAOExecutor} from "noun-contracts/governance/NounsDAOExecutor.sol";
 import {NounsDAOLogicV2} from "noun-contracts/governance/NounsDAOLogicV2.sol";
-import {NounsDAOLogicV3} from "noun-contracts/governance/NounsDAOLogicV3.sol";
 import {NounsDAOProxyV2} from "noun-contracts/governance/NounsDAOProxyV2.sol";
 import {NounsDAOStorageV3} from "noun-contracts/governance/NounsDAOInterfaces.sol";
 import {INounsDAOV2} from "../src/interfaces/INounsDAOV2.sol";
 import {FreeNounsTonken} from "./FreeNounsToken.sol";
 
-contract InitEnvironment is Script {
+contract UpgradeEnvironment is Script {
     NounsDAOProxyV2 proxy = NounsDAOProxyV2(payable(0x461208f0073e3b1C9Cec568DF2fcACD0700C9B7a));
     address constant nounsToken = 0x05d570185F6e2d29AdaBa1F36435f50Bc44A6f17;
     address constant timelock = 0x3daE99d2Fbc2d625f7C9dE5b602C0a78c35d3320;
+    address constant nounsDAOLogicV3 = 0xfd89517098fE86Ee2F2CA208cb7FE7f0B5cD04bd;
 
     uint256 constant VOTING_PERIOD = 40_320; // About 1 week
     uint256 constant VOTING_DELAY = 1;
     uint256 constant PROPOSAL_THRESHOLD = 1;
     uint16 constant QUORUM_VOTES_BPS = 200;
 
-    function run() public returns (NounsDAOLogicV3 nounsDAOLogicV3) {
+    function run() public {
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
-        address deployer = vm.rememberKey(deployerPrivateKey);
 
         vm.startBroadcast(deployerPrivateKey);
 
-        // TODO: figure out correct params
-        NounsDAOStorageV3.NounsDAOParams memory daoParams_ = NounsDAOStorageV3.NounsDAOParams({
-            votingPeriod: VOTING_PERIOD,
-            votingDelay: VOTING_DELAY,
-            proposalThresholdBPS: PROPOSAL_THRESHOLD,
-            lastMinuteWindowInBlocks: 3600,
-            objectionPeriodDurationInBlocks: 360,
-            proposalUpdatablePeriodInBlocks: 3600
-        });
-        NounsDAOStorageV3.DynamicQuorumParams memory dynamicQuorumParams_ = NounsDAOStorageV3.DynamicQuorumParams({
-            minQuorumVotesBPS: QUORUM_VOTES_BPS,
-            maxQuorumVotesBPS: QUORUM_VOTES_BPS,
-            quorumCoefficient: 0
-        });
-
-        nounsDAOLogicV3 = new NounsDAOLogicV3();
-
-        proxy._setImplementation(address(nounsDAOLogicV3));
-
-        // nounsDAOLogicV3(proxy).initialize({
-        //     timelock_: timelock,
-        //     nouns_: nounsToken,
-        //     forkEscrow_: address(0),
-        //     forkDAODeployer_: address(0),
-        //     vetoer_: deployer,
-        //     daoParams_: daoParams_,
-        //     dynamicQuorumParams_: dynamicQuorumParams_
+        // NounsDAOStorageV3.NounsDAOParams memory daoParams_ = NounsDAOStorageV3.NounsDAOParams({
+        //     votingPeriod: VOTING_PERIOD,
+        //     votingDelay: VOTING_DELAY,
+        //     proposalThresholdBPS: PROPOSAL_THRESHOLD,
+        //     lastMinuteWindowInBlocks: 3600,
+        //     objectionPeriodDurationInBlocks: 360,
+        //     proposalUpdatablePeriodInBlocks: 3600
         // });
+        // NounsDAOStorageV3.DynamicQuorumParams memory dynamicQuorumParams_ = NounsDAOStorageV3.DynamicQuorumParams({
+        //     minQuorumVotesBPS: QUORUM_VOTES_BPS,
+        //     maxQuorumVotesBPS: QUORUM_VOTES_BPS,
+        //     quorumCoefficient: 0
+        // });
+
+        proxy._setImplementation(nounsDAOLogicV3);
 
         vm.stopBroadcast();
     }
